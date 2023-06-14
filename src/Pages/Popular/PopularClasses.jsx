f215655caf75d5a622648104d6a2e5f014195104
Shanto57575 <shanto57575@gmail.com>
2023-06-14 23:53:24 +0600

    populer class added

---
+++ src/Pages/Popular/PopularClasses.jsx
@@ -0,0 +1,11 @@
+const PopularClasses = () => {
+	return (
+		<div>
+			<h1 className="text-center text-3xl my-10 text-cyan-500">
+				This is Our Most Enrolled Classes
+			</h1>
+		</div>
+	);
+};
+
+export default PopularClasses;
