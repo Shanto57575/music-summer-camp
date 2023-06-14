237673b9b565cf902061676b2953338559155752
Shanto57575 <shanto57575@gmail.com>
2023-06-14 23:56:25 +0600

    some changes

--- src/Pages/Popular/PopularClasses.jsx
+++ src/Pages/Popular/PopularClasses.jsx
@@ -1,6 +1,9 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../Provider/AuthProvider";
 
 const PopularClasses = () => {
+	const { user, loading } = useContext(AuthContext);
+
 	const [classes, setClasses] = useState([]);
 
 	useEffect(() => {
@@ -8,6 +11,18 @@ const PopularClasses = () => {
 			.then((res) => res.json())
 			.then((data) => console.log(data));
 	}, []);
+
+	if (loading) {
+		return (
+			<div className="text-center my-4">
+				<span className="loading loading-bars loading-xs"></span>
+				<span className="loading loading-bars loading-sm"></span>
+				<span className="loading loading-bars loading-md"></span>
+				<span className="loading loading-bars loading-lg"></span>
+			</div>
+		);
+	}
+
 	return (
 		<div>
 			<h1 className="text-center text-3xl my-10 text-cyan-500">
